591eb3fde952afb933cd9e64796cbaa841e87a64
GLukeM-H <glukemh@gmail.com>
2019-10-24 22:01:00 -0700

    Updates

---
+++ precache-manifest.309ce038af0cb2b67cfaf2cbdbf6d381.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "991bd51aa39865c582874cfffd693a14",
+    "url": "/Demos/index.html"
+  },
+  {
+    "revision": "b8b4cae063ba598fcc8e",
+    "url": "/Demos/static/css/main.5e4e9510.chunk.css"
+  },
+  {
+    "revision": "d373d6d3dad825f32198",
+    "url": "/Demos/static/js/2.4f878980.chunk.js"
+  },
+  {
+    "revision": "b8b4cae063ba598fcc8e",
+    "url": "/Demos/static/js/main.54c43b66.chunk.js"
+  },
+  {
+    "revision": "446d04ec06c75ae24fe4",
+    "url": "/Demos/static/js/runtime-main.34749416.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/Demos/static/media/logo.25bf045c.svg"
+  }
+]);
